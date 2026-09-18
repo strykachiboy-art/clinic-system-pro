57a48d852482224bcdc5d318f984a9900b730120
Stryka-Max <strykachiboy@gmail.com>
2026-09-18 15:59:59 +0200

    refactor(run): remove comment about using socketio.run for Flask-SocketIO

--- run.py
+++ run.py
@@ -5,7 +5,6 @@ from app.extensions import socketio
 app = create_app(os.environ.get("FLASK_ENV", "development"))
 
 if __name__ == "__main__":
-    # socketio.run instead of app.run — required since we're using Flask-SocketIO
     socketio.run(
         app,
         host="0.0.0.0",
